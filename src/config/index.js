064497baa4f8acb045157308b0d314a0e9635379
 Матвей Головко <matweqer@MacBook-Pro-Matvej.local>
2024-05-03 16:26:19 +0300

    feat: add create admin

--- src/config/index.js
+++ src/config/index.js
@@ -3,4 +3,6 @@ import 'dotenv/config';
 export const config = {
   PORT: parseInt(process.env.API_PORT, 10) || 5000,
   JWT_SECRET_KEY: process.env.JWT_SECRET_KEY || 'secret',
+  ADMIN_MAIL: process.env.ADMIN_MAIL || 'test@mail.ru',
+  ADMIN_PASS: process.env.ADMIN_PASS || 'qwerty',
 };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
 import app from './app';
 import { config } from './config';
 import { sequelize } from './models';
+import { AuthController } from './controllers';
 
 async function start() {
   await sequelize.authenticate();
   console.log('Database module initialized');
+}
+
+async function createAdmin() {
+  const user = {
+    email: config.ADMIN_MAIL,
+    password: config.ADMIN_PASS,
+    name: 'Admin',
+    birthday: '1992-02-14',
+    position: 'HR',
+    grade: 'Lead',
+    workProject: 'Human Resources',
+    phone: '123456789',
+    telegram: 'https://t.me/test',
+    office: 'Работает удалённо',
+  };
+  try {
+    await AuthController.register(user);
+    console.log('Super admin created');
+  } catch (e) {
+    console.log('Super admin already exists');
+  }
 }
 
 async function listeningServer() {
@@ -13,6 +35,7 @@ async function listeningServer() {
   });
 }
 start()
+  .then(createAdmin)
   .then(listeningServer)
   .catch((error) => {
     console.error(error);
